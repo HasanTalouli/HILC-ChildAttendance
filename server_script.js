eb207a4f5fb95fc5698e6946b762865b7aa6e445
Tim Tran <ttran23093@gmail.com>
2019-08-27 11:42:31 -0400

    Server setup

---
+++ server_script.js
@@ -0,0 +1,36 @@
+// Tim Tran
+//     CS275
+// Final Project
+
+
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mysql = require("mysql");
+var fs = require("fs");
+
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use(express.static("."));
+
+app.listen(8080,function(){
+    console.log("Server Started");
+});
+
+var con = mysql.createConnection({
+    host:  "localhost",
+    user:  "root",
+    password: pw,
+    database: "school"
+});
+
+con.connect(function(err)  {
+    if (err)  {
+        console.log(err);
+        console.log("Error connecting to database"); 
+    }else  {
+        console.log("Database successfully connected"); 
+    }
+});
