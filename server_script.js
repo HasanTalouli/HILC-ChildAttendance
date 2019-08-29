a89350e4f38b4e2a8538c6d7a8ecdf0be6a301a7
Hasan Talouli <htalouli@gmail.com>
2019-08-29 11:55:47 -0400

    Put Tim's code into my server_script

---
+++ server_script.js
@@ -0,0 +1,69 @@
+var express = require("express");
+var app = express();
+var bodyParser = require("body-parser");
+var mysql = require("mysql");
+var fs = require("fs");
+var builder = require("xmlbuilder");
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+app.use(express.static("./static"));
+
+app.listen(8080,function(){
+    console.log("Server Started");
+});
+
+var config = fs.readFileSync('./config.json');
+var con = mysql.createConnection(JSON.parse(config));
+
+con.connect(function(err)  {
+    if (err)  {
+        console.log(err);
+        console.log("Error connecting to database"); 
+    }else  {
+        console.log("Database successfully connected"); 
+    }
+});
+
+app.get('/getChildren', function(req,res){
+    con.query(`SELECT name, individualID from children`, function(err, rows, field){
+        if (err){
+            console.log(err);
+            console.log("Error querying from the database");
+        }else{
+            res.send(rows);
+            res.end();
+        }
+    })
+})
+
+app.post('/attendance', function(req,res){
+    con.query(`INSERT INTO attendance (individualID, dateOfCare, timeIn, timeOut, attendance, dayOrNight)
+    VALUES
+    (${con.escape(req.body.id)}, ${con.escape(req.body.dateOfCare)}, ${con.escape(req.body.timeIn)}, ${con.escape(req.body.DoB)}, ${con.escape(req.body.major)})`, function(err,rows, field){
+        if (err){
+            console.log(err);
+            console.log("error inserting into database");
+        }else{
+            console.log("added attendance")
+        }
+    })
+})
+
+app.post('/insertChild', function(req,res){
+    con.query(`INSERT INTO child (individualID, caseID, name)
+    VALUES
+    (${con.escape(req.body.individualID)}, ${con.escape(req.body.caseID)}, ${con.escape(req.body.name)}`, function(err,rows, field){
+        if (err){
+            console.log(err);
+            console.log("error inserting into database");
+        }else{
+            console.log("added child")
+        }
+    })
+})
+
+app.get('/getXML', function(req, res){
+    //builder.create('ATT')
+})
